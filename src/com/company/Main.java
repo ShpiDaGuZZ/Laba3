9f7e786346b56b82871e9450811ae6d690b357c1
Sasha <Alexandr>
2021-12-14 01:13:16 +0300

    out

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
-public class MainFrame extends JFrame {
+class MainFrame extends JFrame {
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
 
